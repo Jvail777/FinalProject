0a5de43ad7dc3ded8ac519d879f943df276cd751
Rhianon Hall <hallrhia@gmail.com>
2023-03-19 15:10:41 -0400

    added buttons to leaderboard page but with no functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 // import './css/TriviaCard.css';
 // import './css/Header.css';
 // import { QuestionCard } from './Components/QuestionCard';
-import { Header } from './Components/Header';
-import { LeaderBoard } from './Components/LeaderBoard';
-
-
-
-
+import { Header } from "./Components/Header";
+import { LeaderBoard } from "./Components/LeaderBoard";
+import { Home } from "./Components/Home";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-{/* <QuestionCard question={'Who is the best grand circus staff member?'} answers={["Olaposi","Spencer","Jonathan","Bron"]} correctAnswerIndex={2} onAnswerSelected={function (isCorrect: boolean): void {
+      <div className="App">
+        {/* <QuestionCard question={'Who is the best grand circus staff member?'} answers={["Olaposi","Spencer","Jonathan","Bron"]} correctAnswerIndex={2} onAnswerSelected={function (isCorrect: boolean): void {
       } }/> */}
-      <Header></Header>
-      <Routes>
-        <Route path="/LeaderBoard" element={<LeaderBoard />} />
-      </Routes>
-    </div>
+        <Header></Header>
+
+        <Routes>
+          {/* when click on leaderboard it doesn't show new page - just adds to bottom */}
+          <Route path="/LeaderBoard" element={<LeaderBoard />} />
+          {/* <Route path="*" element={<Navigate to={"/Home"} />}></Route> */}
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- src/Components/LeaderBoard.tsx
+++ src/Components/LeaderBoard.tsx
@@ -1,9 +1,20 @@
-import * as React from 'react';
+import * as React from "react";
+import "../css/LeaderBoard.css";
 
-export function LeaderBoard () {
+export function LeaderBoard() {
   return (
-    <div>
-      <p>LeaderBoard</p>
+    <div className="LB-Header">
+      <h1>LeaderBoard</h1>
+      {/* What appears when button is clicked? New Page? */}
+      <div className="LB-Buttons">
+        <button>Easy</button>
+        <button>Medium</button>
+        <button>Hard</button>
+      </div>
+      <div className="LB-Footer">
+        <button>Home</button>
+        <button>Back</button>
+      </div>
     </div>
   );
 }
