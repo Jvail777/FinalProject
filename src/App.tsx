75bbcfc33c00774f4adb2449b764d0940ae1a6ad
Jvail777 <jvail777@yahoo.com>
2023-03-22 21:53:10 -0400

    Api Calls

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import logo from "./logo.svg";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 // import './css/TriviaCard.css';
 // import './css/Header.css';
-// import { QuestionCard } from './Components/QuestionCard';
+
 
 import { Header } from './Components/Header';
 import { LeaderBoard } from './Components/LeaderBoard';
 import { Login } from './Components/Login';
 import { Home } from "./Components/Home";
 import { Footer } from "./Components/Footer";
-import Dropdown from "./Components/Dropdown";
+import  Trivia  from "./Components/DropDown";
+import { QuestionCard } from "./Components/QuestionCard";
+import DropDown from "./Components/DropDown";
 
 
 
@@ -27,7 +29,8 @@ function App() {
           {/* when click on leaderboard it doesn't show new page - just adds to bottom */}
           <Route path="/LeaderBoard" element={<LeaderBoard />} />
           <Route path="/Login" element={<Login/>}/>
-          <Route path="/Dropdown" element={<Dropdown/>}/>
+          <Route path="/Dropdown" element={<DropDown/>}/>
+          <Route path="/QuestionCard" element={<QuestionCard/>}/>
         </Routes>
         <Footer/>
       </div>
--- src/Components/Dropdown.tsx
+++ src/Components/Dropdown.tsx
@@ -1,33 +1,120 @@
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import '../css/DropDown.css'
+// import { getQuestionsEasy, getQuestionsMedium, getQuestionsHard } from "../Services/QuestionServices";
+// import { QuestionResponse } from "../Models/QuestionModel";
+
+
+// const DifficultyType = [
+//   {
+//     value: 'getQuestionsEasy',
+//     label: 'Easy Questions'
+//   },
+//   {
+//     value: 'getQuestionsMedium',
+//     label: 'Medium Questions'
+//   },
+//   {
+//     value: 'getQuestionsHard',
+//     label: 'Hard Questions'
+//   }
+// ];
+
+
+
+
+// export function DropDown() {
+//   // const [value, setValue] = useState("");
+//   const [selectedOption, setSelectedOption] = useState("");
+
+//   const handleOptionChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+//     setSelectedOption(event.target.value);
+//   };
+
+
+ 
+
+
+
+
+//   return (
+//     <div className="DropDown">
+//       <select className="Categories" value={selectedOption} onChange={handleOptionChange}>
+//         <option value="">Select A Category</option>
+//         <option value="option1">Music</option>
+//         <option value="option2">History</option>
+//         <option value="option3">Film & TV</option>
+//         <option value="option4">General Knowledge</option>
+//         <option value="option5">Society & Culture</option>
+//         <option value="option6">Sports & Leisure</option>
+//       </select>
+
+//       <select className="Difficulty" value={selectedOption} onChange={handleOptionChange}>
+//         <option value="">Select Your Difficulty</option>
+//         <option value="Easy">Easy</option>
+//         <option value="Medium">Medium</option>
+//         <option value="Hard">Hard</option>
+//       </select>
+//     <Link to="/QuestionCard">
+//       <button>Start</button>
+//       </Link>
+//     </div>
+//   );
+// };
+
 import React, { useState } from "react";
-import '../css/DropDown.css'
+import { Link } from "react-router-dom";
 
-const Dropdown = () => {
-  const [selectedOption, setSelectedOption] = useState("");
+interface Question {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
 
-  const handleOptionChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setSelectedOption(event.target.value);
+interface TriviaResponse {
+  response_code: number;
+  results: Question[];
+}
+
+const TRIVIA_API_URL = "https://the-trivia-api.com/api/questions?categories=music&limit=10&region=US&";
+
+const Trivia: React.FC = () => {
+  const [difficulty, setDifficulty] = useState<string>("easy");
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  const handleDifficultyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setDifficulty(event.target.value);
+  };
+
+  const fetchQuestions = async () => {
+    const response = await fetch(`${TRIVIA_API_URL}difficulty=${difficulty}`);
+    const data = await response.json();
+    setQuestions(data.results);
   };
 
   return (
-    <div className="Dropdown">
-      <select className="Categories" value={selectedOption} onChange={handleOptionChange}>
-        <option value="">Select A Category</option>
-        <option value="option1">Music</option>
-        <option value="option2">History</option>
-        <option value="option3">Film & TV</option>
-        <option value="option4">General Knowledge</option>
-        <option value="option5">Society & Culture</option>
-        <option value="option6">Sports & Leisure</option>
+    <div>
+      <select value={difficulty} onChange={handleDifficultyChange}>
+        <option value="easy">Easy</option>
+        <option value="medium">Medium</option>
+        <option value="hard">Hard</option>
       </select>
-
-      <select className="Difficulty" value={selectedOption} onChange={handleOptionChange}>
-        <option value="">Select Your Difficulty</option>
-        <option value="Easy">Easy</option>
-        <option value="Medium">Medium</option>
-        <option value="Hard">Hard</option>
-      </select>
+      <Link to="/QuestionCard">
+      <button onClick={fetchQuestions}>Fetch Questions</button>
+      </Link>
+      {questions.map((question, index) => (
+        <div key={index}>
+          <h3>{question.question}</h3>
+          <p>Difficulty: {question.difficulty}</p>
+          <p>Correct Answer: {question.correct_answer}</p>
+          <p>Incorrect Answers: {question.incorrect_answers.join(", ")}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Dropdown;
+export default Trivia;
