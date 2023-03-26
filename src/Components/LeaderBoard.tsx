f613a1a49c36296de65e8ed172a9c4232d143809
Melissa Morley <melissarose.morley@gmail.com>
2023-03-26 19:11:46 -0400

    leaderboardservice

--- src/Components/LeaderBoard.tsx
+++ src/Components/LeaderBoard.tsx
@@ -3,22 +3,22 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import "../css/LeaderBoard.css";
 import { PlayerModel } from "../Models/PlayerModel";
-// import { getLeaderboard } from "../Services/PlayerServices";
+import { getLeaderboard } from "../Services/PlayerServices";
 
 export function LeaderBoard() {
 
   // const [isSelected, setIsSelected] = useState(false);
   const [leaderboard, setLeaderboard] = useState();
 
-  // let leaderboardHard = getLeaderboard("hard");
-  // console.log(leaderboardHard);
-  // leaderboardHard.then(res => res.map((x) => x.id))
-  // console.log(leaderboardHard);
+  let leaderboardHard = getLeaderboard("hard");
+  console.log(leaderboardHard);
+  // leaderboardHard.then(res => res.map((x) => x.name = "Melissa"))
+  console.log(leaderboardHard);
 
   return (
     <div className="LB-Header">
       <h1>LeaderBoard</h1>
-
+      
       <div className="LB-Buttons">
         <button>Easy</button>
         <button>Medium</button>
--- src/Services/PlayerServices.ts
+++ src/Services/PlayerServices.ts
@@ -7,3 +7,8 @@ export function addPlayer(player:PlayerModel):Promise<PlayerModel>{
     return axios.post<PlayerModel>(`${baseUrl}`, player).then(res => res.data);
 }
 
+export function getLeaderboard(difficulty:string):Promise<PlayerModel[]>{
+    return axios.get<PlayerModel[]>(`${baseUrl}/leaderboard`, {params: {difficulty}})
+    .then(res => (res.data))
+    
+}
