22061860433efe61863d7fd7c36a5221d92d06af
Rhianon Hall <hallrhia@gmail.com>
2023-03-26 15:16:03 -0400

    adjusted more css

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -23,15 +23,15 @@ export function Footer () {
     return (
         <div className="Footer">
             <Link to="/LeaderBoard">
-        <button>LeaderBoards</button>
+        <button className='footer-button'>LeaderBoards</button>
         </Link>
         <Link to="/Dropdown">
-        <button>Reset</button>
+        <button className='footer-button'>Reset</button>
         </Link>
         <Link to="/Game">
-        <button>New Player</button>
+        <button className='footer-button'>New Player</button>
         </Link>
-        <button onClick={toggle}>How To Play</button>
+        <button className='footer-button' onClick={toggle}>How To Play</button>
         <Modal isOpen = {modal} toggle = {toggle} backdrop = {true} backdropClassName = "Modal" centered = {true}>
         <ModalHeader toggle = {toggle} close={closeBtn} >How To Play</ModalHeader>
         <ModalBody contentClassName = "modalBody">Select the category you would like to play. Select a difficulty. Complete all 10 questions to see how many you can get right! Play again to beat your score and move up on the leaderboard! </ModalBody>
