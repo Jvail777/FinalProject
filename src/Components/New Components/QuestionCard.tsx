b47477d140f7c1e0c2128f1b7719b8279a2f9811
Rhianon Hall <hallrhia@gmail.com>
2023-03-27 14:41:54 -0400

    question card styling

--- src/Components/New Components/QuestionCard.tsx
+++ src/Components/New Components/QuestionCard.tsx
@@ -3,77 +3,91 @@ import { Question } from "../../Models/Question";
 import { Card, CardBody, CardTitle } from "reactstrap";
 import "../../css/QuestionCard.css";
 
+interface IQuestionCardProps {
+  questions: Question[];
+  updateScore: Function;
+  onGameEnd: Function;
+}
 
-
-interface IQuestionCardProps{
-    questions: Question[];
-    updateScore: Function;
-    onGameEnd: Function;
-};
-
-export function QuestionCard(props:IQuestionCardProps) {
- 
-
-
+export function QuestionCard(props: IQuestionCardProps) {
   const [questions, setQuestions] = useState<Question[]>([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
   const [answer, setAnswer] = useState<string>("");
- 
 
   function handleNextQuestion() {
     setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
   }
 
-  
-  function onSubmitAnswer(){
-    if(currentQuestionIndex !== 9){
+  function onSubmitAnswer() {
+    if (currentQuestionIndex !== 9) {
       checkAnswer();
       handleNextQuestion();
-      
-    }
-    else{
-      console.log("out of questions")
+    } else {
+      console.log("out of questions");
       props.onGameEnd();
     }
   }
 
-  function checkAnswer(){
-    if(answer === props.questions[currentQuestionIndex].correctAnswer){
-      alert("correct answer")
+  function checkAnswer() {
+    if (answer === props.questions[currentQuestionIndex].correctAnswer) {
+      alert("correct answer");
       props.updateScore();
-    }
-    else{alert("incorrect answer")
+    } else {
+      alert("incorrect answer");
     }
   }
-  
-  
-  let firstAnswer = props.questions[currentQuestionIndex].questionChoices[0]
-  let secondAnswer = props.questions[currentQuestionIndex].questionChoices[1]
-  let thirdAnswer = props.questions[currentQuestionIndex].questionChoices[2]
-  let fourthAnswer = props.questions[currentQuestionIndex].questionChoices[3]
-  
+
+  let firstAnswer = props.questions[currentQuestionIndex].questionChoices[0];
+  let secondAnswer = props.questions[currentQuestionIndex].questionChoices[1];
+  let thirdAnswer = props.questions[currentQuestionIndex].questionChoices[2];
+  let fourthAnswer = props.questions[currentQuestionIndex].questionChoices[3];
+
   return (
-    <div className="QuestionCard">
+    <div className="QuestionCard-Container">
       <h2>Trivia Question:</h2>
-      <Card className="QuestionCard">
-      <CardTitle className="CardTitle">{props.questions[currentQuestionIndex].question}</CardTitle>
-<label>
-  <input type="radio" name="radio" value={firstAnswer.choice} onChange={e => setAnswer(e.target.value)} />
-  {firstAnswer.choice}
-</label>
-<label>
-  <input type="radio" name="radio" value={secondAnswer.choice} onChange={e => setAnswer(e.target.value)} />
-  {secondAnswer.choice}
-</label>
-<label>
-  <input type="radio" name="radio" value={thirdAnswer.choice} onChange={e => setAnswer(e.target.value)} />
-  {thirdAnswer.choice}
-</label>
-<label>
-  <input type="radio" name="radio" value={fourthAnswer.choice} onChange={e => setAnswer(e.target.value)} />
-  {fourthAnswer.choice}
-</label>
-<button onClick={onSubmitAnswer}>Submit Answer</button>
+      <Card className="QuestionCard-Card">
+        <CardTitle className="CardTitle">
+          {props.questions[currentQuestionIndex].question}
+        </CardTitle>
+      
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              value={firstAnswer.choice}
+              onChange={(e) => setAnswer(e.target.value)}
+            />
+            {firstAnswer.choice}
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              value={secondAnswer.choice}
+              onChange={(e) => setAnswer(e.target.value)}
+            />
+            {secondAnswer.choice}
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              value={thirdAnswer.choice}
+              onChange={(e) => setAnswer(e.target.value)}
+            />
+            {thirdAnswer.choice}
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              value={fourthAnswer.choice}
+              onChange={(e) => setAnswer(e.target.value)}
+            />
+            {fourthAnswer.choice}
+          </label>
+          <button onClick={onSubmitAnswer}>Submit Answer</button>
+   
       </Card>
     </div>
   );
