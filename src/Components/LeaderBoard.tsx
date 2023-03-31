2e89dc3e459791a274ee440fc2c5f0e0f0e8c957
Jvail777 <jvail777@yahoo.com>
2023-03-30 21:52:36 -0400

    MobileFirst

--- src/Components/LeaderBoard.tsx
+++ src/Components/LeaderBoard.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "../css/LeaderBoard.css";
+// import "../css/LeaderBoard.css";
 import { PlayerModel } from "../Models/PlayerModel";
 import { getPlayerData} from "../Services/PlayerServices";
 
